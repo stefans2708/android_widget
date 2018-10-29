b8267ec6328427c12133d59f63c521adc1205db9
stefan_sentic <stefan.sentic@htecgroup.com>
2018-10-29 13:38:05 +0100

    change widget look on resize

--- WidgetTest/app/src/main/java/com/android/htec/widgettest/ExampleAppWidgetProvider.java
+++ WidgetTest/app/src/main/java/com/android/htec/widgettest/ExampleAppWidgetProvider.java
@@ -6,7 +6,10 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import static com.android.htec.widgettest.ExampleAppWidgetConfigActivity.KEY_BUTTON_TEXT;
 import static com.android.htec.widgettest.MainActivity.RC_WIDGET_CLICK_ACTION;
@@ -20,6 +23,7 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
      */
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Toast.makeText(context, "onUpdate", Toast.LENGTH_SHORT).show();
 
         for (int appWidgetId : appWidgetIds) {
             Intent intent = new Intent(context, MainActivity.class);
@@ -32,8 +36,45 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
             remoteViews.setOnClickPendingIntent(R.id.button_widget, pendingIntent);
             remoteViews.setCharSequence(R.id.button_widget, "setText", buttonText);
 
+            Bundle appWidgetOptions = appWidgetManager.getAppWidgetOptions(appWidgetId);
+            resizeWidget(appWidgetOptions, remoteViews);
+
             appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
 
     }
+
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        Toast.makeText(context, "onAppWidgetOptionsChanged", Toast.LENGTH_SHORT).show();
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+        resizeWidget(newOptions, views);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    private void resizeWidget(Bundle appWidgetOptions, RemoteViews views) {
+        int maxWidth = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+        int minWidth = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        int minHeight = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+        int maxHeight = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+
+        views.setViewVisibility(R.id.text_widget, maxHeight > 100 ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Toast.makeText(context, "onDeleted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        Toast.makeText(context, "onEnabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        Toast.makeText(context, "onDisabled", Toast.LENGTH_SHORT).show();
+    }
+
 }
