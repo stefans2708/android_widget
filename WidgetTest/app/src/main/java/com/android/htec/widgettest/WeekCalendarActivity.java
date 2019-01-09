8b31f3df9044e4d0c41db05f219b51195403b1e8
stefan_sentic <stefan.sentic@htecgroup.com>
2019-01-09 12:12:31 +0100

    date selection on click and on week change

--- WidgetTest/app/src/main/java/com/android/htec/widgettest/WeekCalendarActivity.java
+++ WidgetTest/app/src/main/java/com/android/htec/widgettest/WeekCalendarActivity.java
@@ -20,19 +20,19 @@ import java.util.Calendar;
 public class WeekCalendarActivity extends AppCompatActivity {
 
     private LinearLayout linearCalendarWeek;
-    private TextView txtMonthName;
+    private TextView txtMonthName, txtSelectedDay;
     private MonthDisplayHelper monthDisplayHelper;
     private int week;
-    private DateTime today;
+    private DateTime selectedDay;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.week_calendar_activity);
         initViews();
-        today = DateTime.now();
-        monthDisplayHelper = new MonthDisplayHelper(today.getYear(), today.getMonthOfYear(), Calendar.MONDAY);
-        week = monthDisplayHelper.getRowOf(today.getDayOfMonth());
+        selectedDay = DateTime.now();
+        monthDisplayHelper = new MonthDisplayHelper(selectedDay.getYear(), selectedDay.getMonthOfYear() - 1, Calendar.MONDAY);
+        week = monthDisplayHelper.getRowOf(selectedDay.getDayOfMonth());
         initDayArray();
         setClickListeners();
         setMonthName();
@@ -41,37 +41,54 @@ public class WeekCalendarActivity extends AppCompatActivity {
     private void initViews() {
         linearCalendarWeek = findViewById(R.id.week_1);
         txtMonthName = findViewById(R.id.text_selected_week);
+        txtSelectedDay = findViewById(R.id.txt_selected_date);
     }
 
     private void initDayArray() {
         LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT);
         buttonParams.weight = 1;
 
-        int[] digitsForWeek = monthDisplayHelper.getDigitsForRow(week);
+        final int[] digitsForWeek = monthDisplayHelper.getDigitsForRow(week);
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Button button = ((Button) v);
-                DateTime dateTag = ((DateTime) button.getTag());
-                if (dateTag.getMonthOfYear() < monthDisplayHelper.getMonth()) {
-                    monthDisplayHelper.previousMonth();
-                } else if (dateTag.getMonthOfYear() > monthDisplayHelper.getMonth()) {
-                    monthDisplayHelper.nextMonth();
-                }
-                setMonthName();
+                selectDay((DateTime) v.getTag());
             }
         };
+
         for (int day = 0; day < 7; day++) {
             Button btnDay = new Button(this);
             btnDay.setLayoutParams(buttonParams);
             btnDay.setSingleLine();
             btnDay.setTextColor(Color.BLUE);
             btnDay.setText(String.valueOf(digitsForWeek[day]));
-            btnDay.setTag(new DateTime(monthDisplayHelper.getYear(), monthDisplayHelper.getMonth(), digitsForWeek[day], 0 ,0));
+            DateTime thisMonth = new DateTime(monthDisplayHelper.getYear(), monthDisplayHelper.getMonth() + 1, 1, 0, 0);
+
+            if (!monthDisplayHelper.isWithinCurrentMonth(week, day)) {
+                if (hasFirstInMonthToRight(digitsForWeek, day)) {       //previous month
+                    btnDay.setTag(thisMonth.minusMonths(1).withDayOfMonth(digitsForWeek[day]));
+                } else { //next month
+                    btnDay.setTag(thisMonth.plusMonths(1).withDayOfMonth(digitsForWeek[day]));
+                }
+            } else {
+                btnDay.setTag(thisMonth.withDayOfMonth(digitsForWeek[day]));
+            }
+
             btnDay.setOnClickListener(onClickListener);
 
             linearCalendarWeek.addView(btnDay);
         }
+    }
+
+    private boolean hasFirstInMonthToRight(int[] weekDays, int selectedDay) {
+        int i = selectedDay + 1;
+        while (i < weekDays.length) {
+            if (weekDays[i] == 1) {
+                return true;
+            }
+            i++;
+        }
+        return false;
     }
 
     private void setClickListeners() {
@@ -82,9 +99,8 @@ public class WeekCalendarActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 linearCalendarWeek.removeAllViews();
-                getNextWeek();
+                selectDay(selectedDay.plusDays(7));
                 initDayArray();
-                setMonthName();
                 Toast.makeText(WeekCalendarActivity.this, "month: " + monthDisplayHelper.getMonth() + "\nweek: " + week, Toast.LENGTH_SHORT).show();
             }
         });
@@ -93,9 +109,8 @@ public class WeekCalendarActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 linearCalendarWeek.removeAllViews();
-                getPrevWeek();
+                selectDay(selectedDay.minusDays(7));
                 initDayArray();
-                setMonthName();
                 Toast.makeText(WeekCalendarActivity.this, "month: " + monthDisplayHelper.getMonth() + "\nweek: " + week, Toast.LENGTH_SHORT).show();
             }
         });
@@ -106,41 +121,17 @@ public class WeekCalendarActivity extends AppCompatActivity {
         txtMonthName.setText(dfs.getMonths()[monthDisplayHelper.getMonth()]);
     }
 
-    private void getPrevWeek() {
-        if (week == 0) {
-            week = 5;
-            monthDisplayHelper.previousMonth();
-            int lastDate = monthDisplayHelper.getDigitsForRow(week)[6];
-            while (lastDate < 15) {
-                week--;
-                lastDate = monthDisplayHelper.getDigitsForRow(week)[6];
+    private void selectDay(DateTime dateTag) {
+        if (selectedDay.getMonthOfYear() != dateTag.getMonthOfYear()) {
+            if (dateTag.isBefore(selectedDay)) {
+                monthDisplayHelper.previousMonth();
+            } else if (dateTag.isAfter(selectedDay)) {
+                monthDisplayHelper.nextMonth();
             }
-        } else {
-            week -= 1;
         }
-
-    }
-
-
-    private void getNextWeek() {
-        if (week == 5) {
-            week = 0;
-            monthDisplayHelper.nextMonth();
-        } else {
-            week += 1;
-        }
-
-        int firstDate = monthDisplayHelper.getDigitsForRow(week)[0];
-
-        if (firstDate == 1) {
-            week = 0;
-            monthDisplayHelper.nextMonth();
-        } else if (week == 5 && firstDate <= 8) {
-            week = 1;
-            monthDisplayHelper.nextMonth();
-        } else if (week == 0 && firstDate >= 26) {
-            week = 1;
-        }
-
+        selectedDay = dateTag;
+        week = monthDisplayHelper.getRowOf(selectedDay.getDayOfMonth());
+        txtSelectedDay.setText(selectedDay.toString());
+        setMonthName();
     }
 }
